8518e50516266ff60e6fb32d94f3b8ad803d6eb9
Arun <arunkushwaha89765@gmail.com>
2023-06-25 14:42:12 +0530

    Scrapped character's build and stats

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const pretty = require('pretty');
 
+const { connectDB } = require('./db');
+const WeaponModel = require('./model/weapon');
+const ArtifactModel = require('./model/artifact');
+
 const url = 'https://genshin.gg/characters/amber/';
 
 const vision = {};
@@ -11,6 +15,7 @@ const weapon = {};
 async function scrapeData() {
  const characterJson = {};
  try {
+  await connectDB();
   const { data } = await axios.get(url);
   const $ = cheerio.load(data);
 
@@ -56,12 +61,85 @@ async function scrapeData() {
     image_url: material.children[0].attribs.src,
    });
   }
+
+  //character build
+  const character_build = {
+   weapon_build: [],
+   artifact_build: [],
+  };
+  const weaponBuild = characterIntro('.character-build')
+   .children()
+   .first()
+   .children(':not(h2)');
+  const artifactBuild = characterIntro('.character-build')
+   .children()
+   .last()
+   .children(':not(h2)');
+
+  for (let i = 0; i < weaponBuild.length; i++) {
+   const weapon = weaponBuild[i];
+   const weaponName = weapon.children[2].children[0].data;
+   const { _id: weaponId } = await WeaponModel.findOne(
+    { name: weaponName },
+    { _id: 1 },
+   );
+   character_build.weapon_build.push({
+    weaponId: weaponId.toString(),
+    rank: i + 1,
+   });
+  }
+
+  for (let i = 0; i < artifactBuild.length; i++) {
+   const artifacts = [];
+   const artifact = artifactBuild[i];
+   const artifactName1 = artifact.children[1].children[1].children[0].data;
+   const { _id: artifactId1 } = await ArtifactModel.findOne(
+    { name: artifactName1 },
+    { _id: 1 },
+   );
+
+   artifacts.push(artifactId1.toString());
+   if (artifact.children[2]) {
+    const artifactName2 = artifact.children[2].children[1].children[0].data;
+    const { _id: artifactId2 } = await ArtifactModel.findOne(
+     { name: artifactName2 },
+     { _id: 1 },
+    );
+    artifacts.push(artifactId2.toString());
+   }
+   character_build.artifact_build.push({
+    artifacts,
+    value: artifacts.map(() => 4 / artifacts.length),
+    rank: i + 1,
+   });
+  }
+
+  //getting character stats
+  const character_stats = [];
+  const characterStats =
+   characterIntro('.character-stats').children(':not(h2)');
+  for (let i = 0; i < characterStats.length; i++) {
+   const stat = characterStats[i];
+   const statName = stat.children[0].children[0].data;
+   let statValue = '';
+   for (let i = 1; i < stat.children.length; i++) {
+    statValue += stat.children[i].data;
+   }
+   character_stats.push({
+    statName,
+    statValue,
+   });
+  }
   characterJson.character_intro = character_intro;
   characterJson.character_materials = character_materials;
+  characterJson.character_build = character_build;
+  characterJson.character_stats = character_stats;
 
   console.log(characterJson);
+  process.exit(-1);
  } catch (error) {
   console.log(error);
+  process.exit(0);
  }
 }
 
